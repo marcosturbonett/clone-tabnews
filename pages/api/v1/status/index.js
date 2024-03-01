f268eb38e4019dbdb76d4eef68f666f1bd72af57
Marcos Oliveira <marcos.turbonett@hotmail.com>
2024-03-01 16:03:35 -0300

    correcoes no banco de dados `try-catch` e finalizacao do `/status` .

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,40 @@
 import database from "infra/database";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum");
-  console.log(result.rows);
-  response.status(200).json({ chave: "teste api" });
+  // data atual
+  const updatedAt = new Date().toISOString();
+
+  //  versao do banco - estudar comando show
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;"
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  // const dbOpenedConnectionsResults = await database.query("SELECT count(*) FROM pg_stat_activity WHERE datname = 'local_db';")
+  // const dbOpenedConnectionsValue = dbOpenedConnectionsResults[0].count
+  // Podemos usar o ::int para já vir do banco convertido.
+  const dabaseOpenedConnectionsResults = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    dabaseOpenedConnectionsResults.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connection: parseInt(databaseMaxConnectionsValue),
+        opened_connections: parseInt(databaseOpenedConnectionsValue),
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,11 @@
 test("get to /api/v1/status/ should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  console.log(responseBody);
+  expect(responseBody.updated_at).toBeDefined();
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
